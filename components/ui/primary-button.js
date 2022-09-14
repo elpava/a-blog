d30f3ef4b911fbbb03ec14a1eb187f9072c69fb6
hadikman <hadikman@gmail.com>
2022-09-14 12:26:37 +0430

    add onClick functionality

--- components/ui/primary-button.js
+++ components/ui/primary-button.js
@@ -1,8 +1,8 @@
 import styles from './primary-button.module.scss';
 
-function PrimaryButton({ text, type = 'button' }) {
+function PrimaryButton({ text, type = 'button', onClick }) {
   return (
-    <button type={type} className={styles.button}>
+    <button type={type} className={styles.button} onClick={onClick}>
       {text}
     </button>
   );
